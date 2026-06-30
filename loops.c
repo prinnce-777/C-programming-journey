e9f8b3d5d0bcbb2cdda4e848a33580a02c2c20ae
Molaka prince enosh <princeenosh80@gmail.com>
2026-06-30 19:36:41 +0530

    Add files via upload

---
+++ loops.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+                     //for loop
+    int i;
+    for(i=1;i<=10;i++){
+        printf("7\n ");      //this loop is used  to print '7' for 10 time(ques.1)
+  }
+
+
+  for (i=0;i<=10;i++){
+    printf("%d \n", i);      //this loop is used to print numbers from 0 to 10(ques.2)
+  }
+
+  for(i=1;i<=10;i++){
+    int ans=0;
+    ans=ans+i;
+  }                         
+   
+  int ans=0;
+ 
+ for(i=0;i<10;i++){
+    ans=ans+i;
+  }
+      printf("%d ",ans);    //this loop is used to print the sum of numbers from 0 to 9(ques.3)
+        return 0;
+  }
